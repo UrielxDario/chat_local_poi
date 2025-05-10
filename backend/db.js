b40e4c4000c8c1e8e47c5414e0ab9ff89ec4a838
Uriel Arguello Grimaldo <uriel.dario.arguello@gmail.com>
2025-05-09 18:32:00 -0600

    Hosteo de bd

--- backend/db.js
+++ backend/db.js
@@ -1,24 +1,26 @@
+require("dotenv").config();
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'DB_POI_Wizard',
-    port: 3306,
-    waitForConnections: true,
-    connectionLimit: 10, // Número máximo de conexiones abiertas al mismo tiempo
-    queueLimit: 0 // Número máximo de conexiones en cola (0 = ilimitado)
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    
+    ssl: {
+        rejectUnauthorized: false,  
+    }, 
 });
+ 
 
-// Probar conexión (opcional pero útil)
-db.getConnection((err, connection) => {
-    if (err) {
-        console.error('Error conectando a MySQL:', err);
-    } else {
-        console.log('Conectado a MySQL con pool');
-        connection.release(); // Liberamos la conexión para que el pool la use luego
-    }
+db.getConnection((err, conn) => {
+  if (err) {
+    console.error("❌ Error al conectar a Clever Cloud:", err);
+  } else {
+    console.log("✅ Conectado a Clever Cloud MySQL");
+    conn.release();
+  }
 });
 
 module.exports = db;
