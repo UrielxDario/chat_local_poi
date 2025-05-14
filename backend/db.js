da5e8b135009d6df8019805d51417ab58706e282
Uriel Arguello Grimaldo <uriel.dario.arguello@gmail.com>
2025-05-13 23:17:14 -0600

    limite de conexiones +

--- backend/db.js
+++ backend/db.js
@@ -7,7 +7,9 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    
+    waitForConnections: true,
+    connectionLimit: 20,   // Aquí puedes subir el límite
+    queueLimit: 0,
     ssl: {
         rejectUnauthorized: false,  
     }, 
