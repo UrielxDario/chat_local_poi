981b0d171c2fca09b8365509b08c71d49e9afe91
luiscar03 <luiscarrizales07@outlook.com>
2025-05-20 10:21:37 -0600

    Subir Fotos A Firebase Prueba (Modificacion A Chats.js, funciones de enviar mensajes)

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,6 +23,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+const storage = getStorage(app);
+
 const db = getFirestore(app);
 
 export { db };
+
+export { storage };
